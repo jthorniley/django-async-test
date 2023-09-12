43fd836eb9ec954b273f6293700aadc8a05cc2ce
James Thorniley <james.thorniley@mixcloud.com>
2023-09-12 15:40:45 +0100

    django entry point error demonstration

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import asyncio
+import time
+from django.http import HttpResponse
+from asgiref.sync import sync_to_async, async_to_sync
+
+
+@sync_to_async
+def sub_task():
+    return time.time()
+
+
+@async_to_sync
+async def task_runner():
+    return await asyncio.create_task(sub_task())
+
 
 # Create your views here.
+def foo(request):
+    return HttpResponse(task_runner())
