3b878c99efa12dd577a0a2d5f0317059434c10e4
James Thorniley <james.thorniley@mixcloud.com>
2023-09-12 19:09:23 +0100

    add model + use it in test

--- app/views.py
+++ app/views.py
@@ -2,11 +2,13 @@ import asyncio
 import time
 from django.http import HttpResponse
 from asgiref.sync import sync_to_async, async_to_sync
+from app.models import MyModel
 
 
 @sync_to_async
 def sub_task():
-    return time.time()
+    instance = MyModel.objects.create(current_time=int(time.time()))
+    return instance.current_time
 
 
 @async_to_sync
--- django_async_test/settings.py
+++ django_async_test/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "django_async_test.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "djangoasynctest",
+        "USER": "djangoasynctest",
+        "PASSWORD": "djangoasynctest",
+        "HOST": "127.0.0.1",
+        "PORT": "3307",
     }
 }
 
