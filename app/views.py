2be771de8d70f5ae897b81be5579372068751533
James Thorniley <james.thorniley@mixcloud.com>
2023-09-13 16:36:54 +0100

    debugging

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import asyncio
 import threading
 import time
+from asgiref.local import Local
 from django.http import HttpResponse
 from asgiref.sync import sync_to_async, async_to_sync
 from app.models import MyModel
@@ -9,7 +10,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def sync_response(request):
+def sync_response(request, asgiref_local):
+    asgiref_local.foo = 4
     logging.info(
         f"REQUEST ID: {id(request):10d} sync_response thread {threading.current_thread()}"
     )
@@ -20,7 +22,8 @@ def sync_response(request):
 
 
 @async_to_sync
-async def async_response(request):
+async def async_response(request, asgiref_local):
+    asgiref_local.foo = 3
     logging.info(
         f"REQUEST ID: {id(request):10d} async_response thread {threading.current_thread()}"
     )
@@ -30,35 +33,48 @@ async def async_response(request):
 
 
 @async_to_sync
-async def task_runner(request):
+async def task_runner(request, asgiref_local):
+    asgiref_local.foo = 2
     logging.info(
         f"REQUEST ID: {id(request):10d} task_runner thread {threading.current_thread()}"
     )
-    return await asyncio.create_task(sync_to_async(sync_response)(request))
+    return await asyncio.create_task(
+        sync_to_async(sync_response)(request, asgiref_local)
+    )
 
 
 # Create your views here.
 def foo(request):
     logging.info(f"REQUEST ID: {id(request):10d} {threading.current_thread()}")
 
+    MyModel.objects.create(current_time=int(time.time()))
+    asgiref_local = Local(thread_critical=True)
+    # asgiref_local.foo = 1
+
     if request.GET.get("safe"):
         # If requested, perform request in pure sync context
         logging.info(f"REQUEST ID: {id(request):10d}   safe option set")
-        response = HttpResponse(sync_response(request))
-        logging.info(f"REQUEST ID: {id(request):10d}   done")
+        response = HttpResponse(sync_response(request, asgiref_local))
+        logging.info(
+            f"REQUEST ID: {id(request):10d}   done - local value: {getattr(asgiref_local, 'foo', 'notset')}"
+        )
         return response
 
     if request.GET.get("notask"):
         # If requested, perform request in async thread without creating a separate task
         logging.info(f"REQUEST ID: {id(request):10d}   no task option set")
-        response = HttpResponse(async_response(request))
-        logging.info(f"REQUEST ID: {id(request):10d}   done")
+        response = HttpResponse(async_response(request, asgiref_local))
+        logging.info(
+            f"REQUEST ID: {id(request):10d}   done - local value: {getattr(asgiref_local, 'foo', 'notset')}"
+        )
         return response
 
     # Default: we wrap the sync_response work function in:
     #  -> async_to_sync(task_runner)   === running in a thread created by async_to_sync
     #  -> asyncio.create_task()        === creates a new asyncio.Task instance
     #  -> sync_to_async(sync_response) === should run in this thread (sync_to_async finds the sync thread above it in the stack)
-    response = HttpResponse(task_runner(request))
-    logging.info(f"REQUEST ID: {id(request):10d}   done")
+    response = HttpResponse(task_runner(request, asgiref_local))
+    logging.info(
+        f"REQUEST ID: {id(request):10d}   done - local value: {getattr(asgiref_local, 'foo', 'notset')}"
+    )
     return response
--- load_test.py
+++ load_test.py
@@ -5,7 +5,7 @@ import pathlib
 
 async def run_test():
     async with AsyncClient(timeout=40) as client:
-        requests = [client.request("GET", "http://localhost:8000/") for _ in range(300)]
+        requests = [client.request("GET", "http://localhost:8000/") for _ in range(200)]
         results = await asyncio.gather(*requests)
 
         for result in results:
